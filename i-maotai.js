690c1492b98ad0bec998a553b838057a0280bd65
xianghuanng <30946500+xianghuanng@users.noreply.github.com>
2023-10-11 10:39:29 +0800

    Update i-maotai.js

--- i-maotai.js
+++ i-maotai.js
@@ -82,7 +82,7 @@ let productInfo = [], message = '', CookieArr = [], Cookie = '', DeviceID = '';
 
 let MT_PROVINCE = $.getdata('MT_PROVINCE') || '湖北省';
 let MT_CITY = $.getdata('MT_CITY') || '武汉市';
-let MT_DISTRICT = $.getdata('MT_DISTRICT') || '';
+let MT_DISTRICT = $.getdata('MT_DISTRICT') || '洪山区|新洲区|武昌区|汉口区|汉阳区';
 let MT_ITEM_BLACK = $.getdata('MT_ITEM_BLACK') || '2478|10056';
 let MT_TOKENS = $.getdata('MT_TOKENS') || '';
 let MT_VERSION = $.getdata('MT_VERSION') || '1.4.9';
